c63e6ee2137ef729b7824253f47e48b335aea2cf
UberPyro <uberpyro181@gmail.com>
2022-10-08 19:15:42 -0400

    Add a few test cases

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,8 @@
+open Batteries
 
+open Jcat.Machine
+open Instructions
+
+(* let () = infer @@ expr [quote @@ expr [int 0]; call] *)
+(* let () = infer @@ expr [int 0; quote @@ expr [int 1; plus]; call; plus] *)
+(* let () = infer @@ expr [bool true; plus] *)
--- test/jcat_test.ml
+++ test/jcat_test.ml
@@ -9,10 +9,20 @@ let pass x = match infer x with
   | exception _ -> false
 
 let tests = [
+  (* true, expr [quote @@ expr [int 0]; call];  *)
+
   true, expr [int 0; int 1; plus]; 
   false, expr [int 0; bool true; plus]; 
   true, expr [int 0; quote @@ expr [int 1; plus]; call]; 
-  false, expr [bool false; quote @@ expr [int 1; plus]; call]
+  false, expr [bool false; quote @@ expr [int 1; plus]; call]; 
+  true, expr [plus]; (* edgecase of this system: main can be whatever; no empty stacks. *)
+  true, expr []; 
+  true, expr [int 0; int 1; quote @@ expr []; call; plus]; 
+  false, expr [int 0; int 1; quote @@ expr []; plus]; 
+  false, expr [int 0; bool false; quote @@ expr []; call; plus]; 
+
+  (* true, expr [quote @@ expr [int 0; int 1; plus]; call];  *)
+  (* true, expr [int 0; quote @@ expr [int 1; plus]; call; plus];  *)
 ]
 
 let same = check Alcotest.bool "same bool"
