87d1bf5b0aa74922fdcc45e7ead3afdc26abee17
UberPyro <uberpyro181@gmail.com>
2022-10-08 19:31:23 -0400

    Leave main blank

--- bin/main.ml
+++
@@ -1,8 +0,0 @@
-open Batteries
-
-open Jcat.Machine
-open Instructions
-
-(* let () = infer @@ expr [quote @@ expr [int 0]; call] *)
-(* let () = infer @@ expr [int 0; quote @@ expr [int 1; plus]; call; plus] *)
-(* let () = infer @@ expr [bool true; plus] *)
