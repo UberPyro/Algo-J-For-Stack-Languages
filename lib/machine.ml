5167628664c4d74b30500af758c1d0c1a2ffe32b
UberPyro <uberpyro181@gmail.com>
2022-10-08 10:44:58 -0400

    Add a lot of comments

--- lib/machine.ml
+++ lib/machine.ml
@@ -5,6 +5,7 @@ open Type
 
 type 'a node = {data : 'a; ty : Type.fn}
 
+(* expression language *)
 type expr = _expr node
 and _expr = word list
 and word = _word node
@@ -18,41 +19,55 @@ and _word =
 let fresh_stack () = 
   uref @@ Bottom (unique ())
 
+(* V => 0 => 0 V *)
 let push_var v s = 
   uref @@ Stack (s, v)
 
+(* Lit => 0 -- 0 Lit *)
 let fresh_lit lit = 
   let s = fresh_stack () in
   s, push_var lit s
 
 let ascribe data ty = {data; ty}
 
+(* provides constructors with correct types *)
 module Instructions = struct
+  (* 0 -- 0 int *)
   let int i = ascribe (Int i) (fresh_lit (uref Type.Int))
   let bool b = ascribe (Bool b) (fresh_lit (uref Type.Bool))
+  (* 0 int int -- 0 int *)
   let plus = ascribe Plus @@
     let s = fresh_stack () in
     s |> push_var (uref Type.Int) |> push_var (uref Type.Int), 
     s |> push_var (uref Type.Int)
+  (* 0 [0 -- 1] -- 1 *)
   let call = ascribe Call @@
     let s0, s1 = fresh_stack (), fresh_stack () in
     push_var (uref @@ Type.Quote (s0, s1)) s0, s1
+  (* (1 -- 2) => 0 -- [1 -- 2] 3 *)
   let quote fn = ascribe (Quote fn) (fresh_lit (uref @@ Type.Quote fn.ty))
+  (* (fn) => fn *)
   let expr e = ascribe e (fresh_stack (), fresh_stack ())
 end
 
+(* some expression `e` has some input type stack `io` and output `o0`. 
+   it is also composed of concatenated subexpressions with their own input and output types. 
+   we must: 
+   - unify `i0` with the input of the leftmost subexpression
+   - unify `o0` with the output of the rightmost subexpression
+   - unify the types of all neighboring subexpressions, so that o(n) = i(n+1) *)
 let rec infer {data; ty=i0, o0} = match data with
-  | [] -> unify_seq i0 o0
+  | [] -> unify_seq i0 o0  (* identity function must have same input and output *)
   | {ty=i, _; _} :: _ -> 
-    unify_seq i0 i; 
+    unify_seq i0 i;  (* i0 is leftmost input *)
     let rec cat = function
       | {ty=_, o1; _} as h :: ({ty=i2, _; _} :: _ as t) -> 
-      infer_word h; 
-      unify_seq o1 i2; 
+      infer_word h;  (* types of subexpressions are resolved *)
+      unify_seq o1 i2; (* neighboring types are unified *)
       cat t
     | [{ty=_, o; _} as x] -> 
       infer_word x; 
-      unify_seq o0 o
+      unify_seq o0 o  (* o0 is rightmost input *)
     | [] -> failwith "Unreachable" in
   cat data
 
--- lib/type.ml
+++ lib/type.ml
@@ -14,29 +14,29 @@ and var_type =
   | Quote of fn
 and seq_ref = seq_type uref
 and seq_type = 
-  | Stack of seq_ref * var_ref
+  | Stack of seq_ref * var_ref  (* stacks have a value on top, or be empty *)
   | Bottom of int  (* initially fresh *)
-and fn = seq_ref * seq_ref
+and fn = seq_ref * seq_ref  (* functions are between stacks *)
   [@@deriving show]
 
 let rec unify_seq r = 
   unite ~sel:begin curry @@ function
     | Bottom _ as s, Bottom _ -> s
-    | Stack _ as s, Bottom _ | Bottom _, (Stack _ as s) -> s
+    | Stack _ as s, Bottom _ | Bottom _, (Stack _ as s) -> s  (* take the longer stack *)
     | Stack (a, t) as s, Stack (b, u) -> 
-      unify_var t u; 
-      unify_seq a b; 
+      unify_var t u;  (* unify the top elements *)
+      unify_seq a b;  (* unify the rest *)
       s
   end r
 
 and unify_var r = 
   unite ~sel:begin curry @@ function
     | Quote (i0, o0) as q, Quote (i1, o1) -> 
-      unify_seq i0 i1; 
-      unify_seq o0 o1; 
+      unify_seq i0 i1; (* unified functions have same inputs *)
+      unify_seq o0 o1; (* and outputs *)
       q
-    | v, Var _ | Var _, v -> v
-    | v, u when v = u -> v
+    | v, Var _ | Var _, v -> v  (* unified var types are the same *)
+    | v, u when v = u -> v  (* unifying something with itself is itself *)
     | v, u -> 
       raise @@ Type_error begin
         Printf.sprintf
